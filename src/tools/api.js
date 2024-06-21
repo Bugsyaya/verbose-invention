3cf5a1a0b15e22ad7ffa33bfd5e7015b2fff0656
Bugsyaya <github@bugsyaya.dev>
2024-06-21 13:33:36 +0200

    :rocket: Deploy

--- src/tools/api.js
+++ src/tools/api.js
@@ -1,5 +1,5 @@
 export const getQuestionsAnswers = async () => {
-  const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/AIzaSyCSBBSCr18D72VeEJjvY9-i1bLyptYbtGo/values/Feuille 1!A1:F50?key=AIzaSyD7k5iZJ_dO6-gqf4T44JkKTfcl0o4acGQ&majorDimension=COLUMNS')
+  const response = await fetch('https://sheets.googleapis.com/v4/spreadsheets/1jfeuGCqFmkyHhdTTin8zXMJeOPhos_9TzfLsvWdZiEs/values/Feuille 1!A1:F50?key=AIzaSyCSBBSCr18D72VeEJjvY9-i1bLyptYbtGo&majorDimension=COLUMNS')
   if (!response.ok) {
     console.error(response.text())
     return
